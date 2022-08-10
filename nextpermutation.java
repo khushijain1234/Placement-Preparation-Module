fe45761e8edc2267793e78d4dae11dc4fd81b8e4
khushijain1234 <78781833+khushijain1234@users.noreply.github.com>
2022-08-10 10:19:37 +0530

    Create nextpermutation.java

---
+++ nextpermutation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+         if(nums==null || nums.length==0)
+            return;
+        int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+        int j=nums.length-1;
+        while(j>=0 && nums[j]<=nums[i]){
+            j--;
+        }
+        swap(nums,i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    void swap(int [] nums,int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    void reverse(int [] nums,int i,int j){
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
