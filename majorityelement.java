bd87c9a7219e3eda86404e0ce0ce4a5eb272b0d8
khushijain1234 <78781833+khushijain1234@users.noreply.github.com>
2022-08-10 10:26:01 +0530

    Create majorityelement.java

---
+++ majorityelement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=1;
+        int temp=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(temp==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count==0){
+                temp=nums[i];
+                count=1;
+            }
+        }
+        return temp;
+    }
+}
